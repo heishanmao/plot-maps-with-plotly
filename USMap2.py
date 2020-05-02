0872b9ec919d1c5455adfe832c44ad1b001d99b9
scott <zx366268928@gmail.com>
2020-05-02 16:02:33 -0400

    new

---
+++ USMap2.py
@@ -0,0 +1,31 @@
+import json
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+
+token = 'pk.eyJ1IjoiaGVpc2hhbm1hbyIsImEiOiJjazlveWkwdnowNHB1M2VuNzJ2dmp5c250In0.kk_ErS2msH2HPAhXOl2i8Q'
+
+with open('.\gz_2010_us_040_00_20m.json') as response:
+    geo = json.load(response)
+
+df = pd.read_csv('.\Production_state_2018.csv', encoding= 'unicode_escape')
+#df = df = pd.read_csv('.\dadadadada.csv')
+
+fig = go.Figure(go.Choropleth(
+    geojson = geo,
+    locations = df.state,
+    z = df.production,
+    featureidkey = 'properties.NAME',
+    #locationmode = 'USA-states',
+    colorscale = 'mint',
+    text = df.state, #hover text!!!
+    marker_line_color = 'white',
+    marker_line_width = 0.5
+))
+
+fig.update_layout(
+    title_text = 'State',
+    geo_scope='usa',
+)
+
+fig.show()
